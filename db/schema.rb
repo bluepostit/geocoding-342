903cbcc93cc9b33018cc1c533a1006b5970c37d2
bluepostit <32864499+bluepostit@users.noreply.github.com>
2020-02-18 10:10:45 +0200

    show flats on map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_071259) do
+ActiveRecord::Schema.define(version: 2020_02_18_071803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_02_18_071259) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
